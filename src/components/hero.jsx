9e83c868ad84bd423b5b3bd48cb16c4b1992c46f
Callum Harrod <callumharrod1994@hotmail.co.uk>
2025-02-17 15:14:34 +0000

    Added more details and styling to the hero component

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,41 +1,99 @@
+// import { cva } from "class-variance-authority";
 import PropTypes from "prop-types";
 
+// const heroVariants = cva("mx-auto flex w-full max-w-[1360px]", {
+//   variants: {
+//     layout: {
+//       leftAligned: "items-center justify-between",
+//       centered: "xxx",
+//     },
+//   },
+//   defaultVariants: {
+//     intent: "primary",
+//     spacing: "relaxed",
+//   },
+// });
+
 const Hero = ({
+  // layout,
   preHeading,
   heading,
+  // headingSize,
   text,
+  // textColor,
   button1Label,
   button1Link,
+  // button1Style,
   button2Label,
   button2Link,
+  // button2Style,
+  // image,
   backgroundImage,
+  // darkenImage,
+  // backgroundColor,
 }) => {
   return (
     <div
-      className="align-center flex h-[672px] w-full justify-start bg-cover bg-center bg-no-repeat"
+      className="align-center flex h-[672px] w-full justify-start bg-cover bg-center bg-no-repeat px-32"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
-      <div className="mx-auto w-full max-w-[1360px] px-4 py-12">
-        {preHeading && <div className="text-sm uppercase">{preHeading}</div>}
-        {heading && <h1 className="text-3xl font-bold">{heading}</h1>}
-        {text && <p className="mt-4">{text}</p>}
-        <div className="mt-8">
-          {button1Label && (
-            <a
-              href={button1Link}
-              className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-            >
-              {button1Label}
-            </a>
+      <div className="mx-auto flex w-full max-w-[1360px] items-center justify-between">
+        <div className="flex max-w-xl flex-col items-start justify-start gap-4">
+          {preHeading && (
+            <div className="text-lg font-bold text-blue-700">{preHeading}</div>
           )}
-          {button2Label && (
-            <a
-              href={button2Link}
-              className="ml-4 rounded border border-blue-500 bg-transparent px-4 py-2 text-blue-700 hover:bg-blue-500 hover:text-white"
-            >
-              {button2Label}
-            </a>
+          {heading && (
+            <h1 className="mb-4 text-6xl font-bold text-slate-950">
+              {heading}
+            </h1>
           )}
+          {text && <p className="mb-4 text-lg text-slate-950">{text}</p>}
+          <div className="flex gap-4">
+            {button1Label && (
+              <a
+                href={button1Link}
+                className="inline-flex items-center gap-x-2 rounded-lg border border-transparent bg-blue-600 px-4 py-3 text-sm font-medium text-white hover:bg-blue-700 focus:bg-blue-700 focus:outline-none disabled:pointer-events-none disabled:opacity-50"
+              >
+                {button1Label}
+                <svg
+                  width="14"
+                  height="14"
+                  viewBox="0 0 14 14"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M5.25 10.5L8.75 7L5.25 3.5"
+                    stroke="white"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </a>
+            )}
+            {button2Label && (
+              <a
+                href={button2Link}
+                className="inline-flex items-center gap-x-2 rounded-lg border border-blue-600 px-4 py-3 text-sm font-medium text-blue-600 hover:border-blue-600 hover:text-blue-600 focus:border-blue-600 focus:text-blue-600 focus:outline-none disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-700 dark:text-neutral-400 dark:hover:border-blue-600 dark:hover:text-blue-500 dark:focus:border-blue-600 dark:focus:text-blue-500"
+              >
+                {button2Label}
+                <svg
+                  width="14"
+                  height="14"
+                  viewBox="0 0 14 14"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M5.25 10.5L8.75 7L5.25 3.5"
+                    stroke="#2563EB"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </a>
+            )}
+          </div>
         </div>
       </div>
     </div>
@@ -43,25 +101,41 @@ const Hero = ({
 };
 
 Hero.propTypes = {
+  layout: PropTypes.string,
   preHeading: PropTypes.string,
   heading: PropTypes.string,
+  headingSize: PropTypes.string,
   text: PropTypes.string,
+  textColor: PropTypes.string,
   button1Label: PropTypes.string,
   button1Link: PropTypes.string,
+  button1Style: PropTypes.string,
   button2Label: PropTypes.string,
   button2Link: PropTypes.string,
+  button2Style: PropTypes.string,
+  image: PropTypes.string,
   backgroundImage: PropTypes.string,
+  darkenImage: PropTypes.string,
+  backgroundColor: PropTypes.string,
 };
 
 Hero.defaultProps = {
+  layout: "",
   preHeading: "",
   heading: "",
+  headingSize: "",
   text: "",
+  textColor: "",
   button1Label: "",
   button1Link: "",
+  button1Style: "",
   button2Label: "",
   button2Link: "",
+  button2Style: "",
+  image: "",
   backgroundImage: "",
+  darkenImage: "",
+  backgroundColor: "",
 };
 
 export default Hero;
